b723d2b11dc0eb7342c602eb9ca1001d37701fbf
Mike <michailkasmeridis@gmail.com>
2024-03-30 22:13:11 +0000

    Changed sensor data to struct for easier handling

--- master_algorithm/main/master_algorithm.c
+++ master_algorithm/main/master_algorithm.c
@@ -10,6 +10,17 @@
 
 float distance_left, distance_right;
 
+struct SensorData{
+    float us_distance_left, us_distance_right;
+};
+
+struct SensorData get_measurements(){
+    struct SensorData data;
+    data.us_distance_left = ultrasonic_scan(LEFT_TRIGGER, LEFT_ECHO);
+    data.us_distance_right = ultrasonic_scan(RIGHT_TRIGGER, RIGHT_ECHO);
+    return data;
+};
+
 void initialisations(){
     // gpio_set_direction(MOTOR,GPIO_MODE_OUTPUT);
     // gpio_set_level(MOTOR,0); 
@@ -21,13 +32,12 @@ void initialisations(){
 
 // }
 
-void get_measurements(void *pvParameters){
+void main_app(void *pvParameters){
     initialisations();
     while(true){
-        distance_left = ultrasonic_scan(LEFT_TRIGGER, LEFT_ECHO);
-        distance_right = ultrasonic_scan(RIGHT_TRIGGER, RIGHT_ECHO);
-        printf("dist left: %0.04f cm\n", distance_left*100);                  // display the result to terminal 
-        printf("dist right: %0.04f cm\n", distance_right*100);                  // display the result to terminal 
+        struct SensorData data = get_measurements();
+        printf("dist left: %0.04f cm\n", data.us_distance_left*100);                  // display the result to terminal 
+        printf("dist right: %0.04f cm\n", data.us_distance_right*100);                  // display the result to terminal 
         vTaskDelay(pdMS_TO_TICKS(1000));                            // repeat every 1000ms=1s
     }
 }
@@ -38,7 +48,7 @@ void get_measurements(void *pvParameters){
 // }
 
 void app_main(){
-    xTaskCreate(get_measurements, "components_measurement", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);}
+    xTaskCreate(main_app, "components_measurement", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);}
                                                                     // create a task for freeRTOS to manage,
                                                                     // identifier name "ultrasonic_scan", 
                                                                     // stack size=3*16kB
